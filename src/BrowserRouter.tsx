59863252e86758d2c4a1f9869e5117c90b5b2a2e
dang2608 <gianggiangg2608@gmail.com>
2025-08-26 11:50:21 +0700

    feat: add login UI

--- src/BrowserRouter.tsx
+++ src/BrowserRouter.tsx
@@ -6,12 +6,21 @@ import {
   Route,
 } from "react-router-dom";
 
+const Signup = lazy(() => import("./pages/authentication/Signup"));
 const Login = lazy(() => import("./pages/authentication/Login"));
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" loader={() => redirect("/login")} />
+      <Route
+        path="/signup"
+        element={
+          <Suspense fallback={<p>Loading...</p>}>
+            <Signup />
+          </Suspense>
+        }
+      />
       <Route
         path="/login"
         element={
